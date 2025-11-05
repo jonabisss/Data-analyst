cc8dfa079929892a52884d0d7ea2af32278b1848
jonabisss <jonabisss@gmail.com>
2025-11-05 16:13:35 +0200

    addfiles

--- main.py
+++ main.py
@@ -7,8 +7,4 @@ age=int(input("Enter your age: "))
 gender=str(input("Enter you gender: "))
 
 #display to the user
-<<<<<<< HEAD
-print(f"Hello, {name} your age is {age} and your gender is {gender}" )
-=======
-print(f"Hello, welcome {name} your age is {age} and your gender is {gender}" )
->>>>>>> b5cb615b9fc56150a2b9f03d26d1736204b36535
+print(f"Hello, Wish you very welcome {name}, your age is {age} and you let us know that your gender is {gender}", all the best )
